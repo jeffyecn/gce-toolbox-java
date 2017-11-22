03338284269ef24ac749ab5467c5a64adc78315e
Jeff Ye <jeffyecn@gmail.com>
2017-11-22 17:59:15 -0500

    basic class

--- library/src/main/ca/jeffyecn/gcetoolbox/EnvDetector.java
+++ library/src/main/ca/jeffyecn/gcetoolbox/EnvDetector.java
@@ -4,5 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EnvDetector {
+
     private final static Logger LOG = LoggerFactory.getLogger(EnvDetector.class);
+
+    private final static EnvDetector instance = new EnvDetector();
+
+    public static EnvDetector getInstance() {
+        return instance;
+    }
+
+    private EnvDetector() {
+
+    }
+
+    public boolean runningInGCE() {
+        return false;
+    }
 }
