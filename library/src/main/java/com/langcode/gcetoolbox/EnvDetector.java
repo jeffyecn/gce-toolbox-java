d87bc71de28b56a0ed860b31f4456f79bd8b6304
Jeff Ye <jeffyecn@gmail.com>
2017-11-24 14:53:24 -0500

    project structure

--- library/src/main/java/com/langcode/gcetoolbox/EnvDetector.java
+++ library/src/main/java/com/langcode/gcetoolbox/EnvDetector.java
@@ -3,6 +3,10 @@ package com.langcode.gcetoolbox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 public class EnvDetector {
 
     private final static Logger LOG = LoggerFactory.getLogger(EnvDetector.class);
@@ -13,8 +17,33 @@ public class EnvDetector {
         return instance;
     }
 
+    private Timer timer = null;
+
     private EnvDetector() {
 
+    }
+
+    public void detect() {
+        LOG.info("detect env");
+    }
+
+    public void enableAutoRefresh(long interval, TimeUnit timeUnit) {
+        if ( timer != null ) {
+            LOG.warn("auto refresh already enabled");
+        }
+        timer = new Timer("env refresh time", true);
+        long period = timeUnit.toMillis(interval);
+        LOG.info("period {}", period);
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    detect();
+                } catch (Exception ex) {
+                    LOG.error("Refresh env failed.");
+                }
+            }
+        }, period, period);
     }
 
     public boolean runningInGCE() {
--- test-app/src/main/java/com/langcode/gcetoolbox/TestApp.java
+++ test-app/src/main/java/com/langcode/gcetoolbox/TestApp.java
@@ -3,6 +3,8 @@ package com.langcode.gcetoolbox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.TimeUnit;
+
 public class TestApp {
     private final static Logger LOG;
 
@@ -10,7 +12,11 @@ public class TestApp {
         LOG = LoggerFactory.getLogger(TestApp.class);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         LOG.info("testing");
+
+        EnvDetector detector = EnvDetector.getInstance();
+        detector.detect();
+        detector.enableAutoRefresh(1, TimeUnit.MINUTES);
     }
 }
